795a030465f1ef9e59b86ff5e3d32dc63c290630
Sugam <sugams342@gmail.com>
2024-09-10 15:53:37 +0530

    added configuration stuff

---
+++ {{cookiecutter.project_slug}}/run_once.sh
@@ -0,0 +1,11 @@
+github_url="https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.project_slug}}"
+git init
+git remote add origin github_url
+
+poetry env use /usr/local/bin/python3.12
+poetry add black isort flake8 --group formatting
+poetry add mypy --group typechecking
+poetry add pytest pytest-cov --group testing
+poetry add pre-commit
+poetry run pre-commit install -t pre-commit
+poetry run pre-commit install -t pre-push

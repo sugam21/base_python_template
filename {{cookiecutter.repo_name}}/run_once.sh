91b7db4ed72b247fea41aac839c5c3b2b656392a
Sugam <sugams342@gmail.com>
2024-09-10 17:09:24 +0530

    fixed pre-commit

--- {{cookiecutter.repo_name}}/run_once.sh
+++ {{cookiecutter.repo_name}}/run_once.sh
@@ -2,10 +2,9 @@ github_url="https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.r
 git init
 git remote add origin github_url
 
-poetry env use /usr/local/bin/python3.12
+poetry add pre-commit
+pre-commit install
+
 poetry add black isort flake8 --group formatting
 poetry add mypy --group typechecking
 poetry add pytest pytest-cov --group testing
-poetry add pre-commit
-poetry run pre-commit install -t pre-commit
-poetry run pre-commit install -t pre-push
